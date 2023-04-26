da0641b7680076f2a7b9f73392bee8328037bf82
MaRafaBe <116843246+MaRafaBe@users.noreply.github.com>
2023-04-26 22:34:47 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 08MBID---Visualizacion-de-Datos
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-3gvuxu)
---
+++ index.js
@@ -0,0 +1,5 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
